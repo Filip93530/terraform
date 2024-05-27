e891979cd5a7d30592898e3f5d18b3f2ea294ec3
Filip93530 <152792813+Filip93530@users.noreply.github.com>
2024-05-27 18:31:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "eu-central-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_region_validation      = true
+  s3_use_path_style           = true
+  endpoints {
+    s3 = "http://localhost:4566"
+  }
+}
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "mys3bucket"
-
-  }
+}
